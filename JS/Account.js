682683d9e926e3afc933ef44c6ede5e3c9517693
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-01-18 09:28:54 -0500

    3.1.POOJS

--- JS/Account.js
+++ JS/Account.js
@@ -1,8 +1,12 @@
-function Account(name, document) {
-    this.id;
-    this.name = name;
-    this.document = document;
-    this.email;
-    this.password;
+class Account {
+
+    constructor(name, document) {
+        this.id;
+        this.name = name;
+        this.document = document;
+        this.email;
+        this.password;
+    }
+    
 
 }
