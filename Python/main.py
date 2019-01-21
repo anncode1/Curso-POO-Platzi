26f482cf5a36edabed3eff25a9ddd20e5103a530
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-01-21 01:15:39 -0600

    Constructor Python

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,10 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+    
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Matha"
-    print(vars(car2))
